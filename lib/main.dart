09f3515c9aea8ef9d14c4f1b7c8a6ec67c4a9ac4
stepkj <karthikrpj2002@gmail.com>
2021-09-08 15:48:42 +0530

    new app made (pre-anonymous sign_in) without andorid x apparently

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:tracker_app_kj/app/sign_in/sign_in_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'Time Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: SignInPage(),
+    );
   }
 }
